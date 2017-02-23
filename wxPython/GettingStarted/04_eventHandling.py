3e14e9b73d1e036c6ddb25bbae9fa6858ae04ba0
Binlong Li <bil@microsoft.com>
2017-02-22 23:54:34 -0800

    wxPython learning from getting started

---
+++ wxPython/GettingStarted/04_eventHandling.py
@@ -0,0 +1,31 @@
+import wx
+
+class MainWindow(wx.Frame):
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title = title, size = (200, 100))
+        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.CreateStatusBar()
+        filemenu = wx.Menu()
+        menuAbout = filemenu.Append(wx.ID_ABOUT, "&About", " Information about this program")
+        filemenu.AppendSeparator()
+        menuExit = filemenu.Append(wx.ID_EXIT, "E&xit", " Terminate the program")
+
+        #Creating the menubar
+        menuBar = wx.MenuBar()
+        menuBar.Append(filemenu, "&File")
+        self.SetMenuBar(menuBar)
+        self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
+        self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
+        self.Show(True)
+
+    def OnAbout(self, event):
+        dlg = wx.MessageDialog(self, "A small text editor", "About Sample Editor", wx.OK)
+        dlg.ShowModal() #show it
+        dlg.Destroy()
+
+    def OnExit(self, event):
+        self.Close(True)
+
+app = wx.App(False)
+frame = MainWindow(None, "Sample Editor")
+app.MainLoop()
---
+++ wxPython/readme.txt
@@ -0,0 +1,4 @@
+https://wiki.wxpython.org/Getting%20Started
+
+List of Events:
+https://wiki.wxpython.org/ListOfEvents
